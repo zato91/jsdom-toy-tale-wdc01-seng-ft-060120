65b8612ea8e394d03b7fc4e59e46bb9112659f6e
zato91 <zatokey@gmail.com>
2020-07-16 17:43:35 -0400

    finsihed 2nd deliverable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 let addToy = false;
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys();
+  let submitButton = document.getElementById('toy-form');
+
+  submitButton.addEventListener('submit', event => newToy(event));
+  debugger
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +20,49 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+function fetchToys() {
+   
+  fetch('http://localhost:3000/toys/')
+    .then(response => response.json())
+    .then(data => displayToys(data));
+
+}
+
+function displayToys(data) {
+  const toyCollection = document.getElementById("toy-collection");
+  data.forEach(element => {
+    console.log(element.name);
+    const toyCard = document.createElement("div");
+    toyCard.className = "card";
+    toyCard.innerHTML = `
+    <h2>${element.name}</h2>
+    <img alt="toy picture"
+    src= "${element.image}" class="toy-avatar" />
+    <p>this toy has ${element.likes} likes </p>
+    <button class="like-btn">click to like</button>
+    `
+    toyCollection.append(toyCard);
+  });
+}
+
+ 
+function newToy(e){
+  
+  e.preventDefault();
+  let getForm = document.querySelector('form');
+  fetch('http://localhost:3000/toys/', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: getForm.name.value,
+      image: getForm.image.value,
+      likes: 0
+    })
+  });
+}
+
